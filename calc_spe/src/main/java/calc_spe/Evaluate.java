ec64e374cc6920b7fc0a1af256fd57f0deebab7e
gurleen-kaur-30 <gurleen.kaur@iiitb.org>
2021-03-18 00:49:36 +0530

    added log4j

--- calc_spe/src/main/java/calc_spe/Evaluate.java
+++ calc_spe/src/main/java/calc_spe/Evaluate.java
@@ -1,8 +1,11 @@
 package calc_spe;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.lang.Math;
 import java.math.BigInteger;
 
 public class Evaluate {
+	private static final Logger logger = LogManager.getLogger(calc_spe.Evaluate.class);
 	public static BigInteger factorial(BigInteger number) {
 	    BigInteger result = BigInteger.valueOf(1);
 
@@ -15,10 +18,12 @@ public class Evaluate {
 
 	public static double evaluate(String text) {
 		if(text.charAt(0) == '√') {
+			logger.info("Computing square root of " +Double.parseDouble(text.substring(1,text.length())));
 			return Math.sqrt(Double.parseDouble(text.substring(1,text.length())));
 		} 
 		
 		else if(text.charAt(0) == 'l') {
+			logger.info("Computing Log of " +text.substring(2,text.length()));
 			return Math.log(Double.parseDouble(text.substring(2,text.length())));
 		}
 		
@@ -31,11 +36,14 @@ public class Evaluate {
                 }
 
 				else if(text.charAt(i) == '^' ) {
+					logger.info("Computing " +text.substring(0, i) + "raised to the power of" +Double.parseDouble(text.substring(i+1)));
 					return Math.pow(Double.parseDouble(text.substring(0, i)), Double.parseDouble(text.substring(i+1)));
 				}
 				
 				else if(text.charAt(i) == '!') {
+			
 			        BigInteger bigIntegerStr=new BigInteger(text.substring(0,i));
+					logger.info("Computing factorial of " +bigIntegerStr.doubleValue());
  					return factorial(bigIntegerStr).doubleValue();
 				}
 				
